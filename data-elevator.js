f4a5c4a0d0c2d93e6c5234e25e513495c1906741
wei <124412877+wei7122@users.noreply.github.com>
2023-03-28 16:02:26 +0800

    Add files via upload

---
+++ data-elevator.js
@@ -0,0 +1,24 @@
+/*
+ ** TODO: 電梯DATA ******************
+ */
+// 小網下方固定按鈕圖片位置(left: 左圖右文 top: 上圖下文)
+var iconPosition = "top";
+
+// 小網下方固定按鈕(文案、連結;請不要超過五組)
+var iconName = [
+  {
+    "linkUrl": "#note-top",
+    "imageUrl": "icon-1.png",
+    "words": "活動辦法",
+  },
+  {
+    "linkUrl": "#banner-top",
+    "imageUrl": "icon-2.png",
+    "words": "優惠活動",
+  },
+  {
+    "linkUrl": "#hot-top",
+    "imageUrl": "icon-3.png",
+    "words": "熱門館別",
+  },
+]
